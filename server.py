b2c7472339ccccfd1c67fb886bb83619b581fc35
rikitrentini <111927349+rikitrentini@users.noreply.github.com>
2025-06-05 19:16:43 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, request, jsonify
+import json
+import os
+
+app = Flask(__name__)
+ORDERS_FILE = 'orders.json'
+
+def save_order(data):
+    with open(ORDERS_FILE, 'w') as f:
+        json.dump(data, f)
+
+def load_order():
+    if not os.path.exists(ORDERS_FILE):
+        return {}
+    with open(ORDERS_FILE, 'r') as f:
+        return json.load(f)
+
+@app.route('/master/send_order', methods=['POST'])
+def send_order():
+    data = request.get_json()
+    save_order(data)
+    return jsonify({'status': 'ok'})
+
+@app.route('/slave/fetch_order', methods=['GET'])
+def fetch_order():
+    return jsonify(load_order())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
